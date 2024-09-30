a3ce0936870b1bc436a1e9cdf0acd9231bded7c9
patricio <pzepeda@yahoo.com>
2024-09-29 23:20:33 -0300

    agrego archivos

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,9 @@
+function example(a, b, c){
+    return a+b+c
+}
+
+//funcion de expresion
+
+const example = function(a, b, c) {
+    return a+b+c
+}
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//
+    //function pintar(){
+    //    ele.style.backgroundColor = 'yellow'
+    //}
+    // const ele = document.getElementById("ele1")
+    // ele.addEventListener("click, pintar");
+    
+    // Funcion Anónima
+
+    const ele = document.getElementById("ele")
+    ele.addEventListener("click", function() {
+        ele.style.backgroundColor = 'yellow'
+    })
